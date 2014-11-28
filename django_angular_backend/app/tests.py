202c99631ee6104f7a7219c8f929b1d6ac337d89
Ruslan Makarenko <ruslan.makarenko@gmail.com>
2014-11-28 13:38:02 +0200

    ticket:1 Added MyInfoTest. Test fail

--- django_angular_backend/app/tests.py
+++ django_angular_backend/app/tests.py
@@ -1,22 +1,7 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
 from django.core.urlresolvers import reverse
 from django.test import TestCase
-from django.test.client import Client
-
-
-
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+from django.test import LiveServerTestCase
+from selenium import webdriver
 
 
 class HttpTest(TestCase):
@@ -24,4 +9,35 @@ class HttpTest(TestCase):
         c = Client()
         response = c.get(reverse('home'))
         self.assertEqual(response.status_code, 200)
-        self.assertContains(response, 'Hello')
+        self.assertContains(response, '42-test-angular')
+
+class MyInfoTest(LiveServerTestCase):
+
+    def setUp(self):
+        self.browser = webdriver.PhantomJS()
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_render_info_on_main_page(self):
+        myInfo = [
+            ('First Name',    'Ruslan'),
+            ('Last Name',     'Makarenko'),
+            ('Date of birth', '01.12.1986'),
+            ('Email',         'ruslan.makarenko@gmail.com'),
+            ('Jabber',        'macruss@jabber.kiev.ua')
+        ]
+
+        self.browser.get(self.live_server_url)
+        heading = self.browser.find_element_by_tag_name('h2')
+        self.assertEquals(heading.text, 'My info')
+
+        contact_fields = self.browser.find_elements_by_tag_name('tr')
+
+        for i in range(len(myInfo)):
+            name_field = contact_fields[i].find_elements_by_tag_name('td')[0].text
+            value = contact_fields[i].find_elements_by_tag_name('td')[1].text
+
+            self.assertEquals(myInfo[i][0], name_field)
+            self.assertEquals(myInfo[i][1], value)
